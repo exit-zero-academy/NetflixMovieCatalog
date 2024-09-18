964d9bf3f7479e1151872555b29e288e482799e7
Chanchreek <145703811+Chanchreek@users.noreply.github.com>
2024-09-18 14:20:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
+#!/bin/bash
+set -e
 
+echo ""
+echo "Installing python3.12-venv..."
+echo "--------------------------------"
+sudo apt-get update 
+sudo apt-get install -y python3.12-venv
+
+echo ""
+echo "Creating a Python virtual environment..."
+echo "--------------------------------"
+python3 -m venv venv
+
+echo ""
+echo "Activating the Python virtual environment..."
+echo "--------------------------------"
+source venv/bin/activate
+
+echo ""
+echo "Install Python dependencies..."
+echo "--------------------------------"
+pip install -r requirements.txt
+
+echo ""
+echo "Starting the Python application..."
+echo "--------------------------------"
+python3 app.py
 # TODO your deploy script implementation...

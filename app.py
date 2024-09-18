8d4b63557c5da7294cfe48394260fea1170798fa
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2024-09-18 15:10:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with open('data/data_movies.json', 'r') as f:
 
 @app.route("/", methods=['GET'])
 def home():
-    return "Hi! This app is an API, there is no UI ;-)"
+    return "Hello This app is an API, there is no UI ;-)"
 
 
 @app.route('/discover')

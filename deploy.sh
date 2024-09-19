023424d6f17f99edefe1ee431cda4b03dfee85b6
lavanya-arora <arora.lavanya08@gmail.com>
2024-09-19 23:50:26 +0530

    -

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
+set -e
 
-# TODO your deploy script implementation...
+# Variables
+PORT=8080
+
+echo ""
+echo "Installing python3.12-venv..."
+echo "--------------------------------"
+sudo apt-get update
+sudo apt-get install -y python3.12-venv
+
+echo ""
+echo "Creating a Python virtual environment..."
+echo "--------------------------------"
+python3 -m venv venv
+
+echo ""
+echo "Activating the Python virtual environment..."
+echo "--------------------------------"
+source venv/bin/activate
+
+echo ""
+echo "Installing Python dependencies..."
+echo "--------------------------------"
+pip install -r requirements.txt
+
+echo ""
+echo "Stopping any process using port $PORT..."
+echo "--------------------------------"
+sudo fuser -k $PORT/tcp || true  # Kill process using the port, suppress errors if non
+
+echo ""
+echo "Starting the Python application..."
+echo "--------------------------------"
+python app.py
